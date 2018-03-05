3d6e302d60e412de9472b59667e51ff203ba4f00
MorganTHobson <morganthobson@gmail.com>
2018-03-05 18:33:39 -0500

    coinFlip works, still need timer

--- CoinFlip.java
+++ CoinFlip.java
@@ -1,28 +1,70 @@
 import java.util.Random;
 
-
-class Coin
+class CoinFlip implements Runnable
 {
-    private Random rand;
+  private Random rand;
 
-    public Coin()
+  private static int num_flips;
+  private static int heads = 0;
+
+  public CoinFlip()
+  {
+    rand = new Random();
+  }
+
+  public void run()
+  {
+    int flip_counter;
+    while(true)
     {
-        this.rand = new Random();
-    }
+      synchronized(CoinFlip.class)
+      {
+        flip_counter = num_flips--;
+      }
 
-    public boolean flip()
-    {
-        return this.rand.nextBoolean();
-    }
-}
+      if (flip_counter <= 0) {return;}
 
-public static void main (String[] args)
-{
+      if (rand.nextBoolean())
+      {
+        synchronized(CoinFlip.class){++heads;}
+      }
+        
+    }
+  }
+
+  public static void main (String[] args)
+  {
     if (2 != args.length)
     {
-        System.out.println("Usage: java CoinFlip #threads #flips");
-        return;
+      System.out.println("Usage: java CoinFlip #threads #flips");
+      return;
     }
 
-    
+    int num_threads = Integer.parseInt(args[0]);
+
+    num_flips = Integer.parseInt(args[1]);
+
+    Thread[] threads = new Thread[num_threads];
+
+    for ( int i=0; i<num_threads; i++ )
+    {
+      threads[i] = new Thread ( new CoinFlip() );
+      threads[i].start();
+    }
+
+    for ( int i=0; i<num_threads; i++ )
+    {
+      try
+      {
+        threads[i].join();
+      }
+      catch (InterruptedException e)
+      {
+         System.out.println("Thread interrupted.  Exception: " + e.toString() +
+                           " Message: " + e.getMessage()) ;
+        return;
+      }
+    }
+    System.out.printf("%d heads in %s coin tosses\n", heads, args[1]);
+  }
 }
